a1d93a68a7f35887292e5287bc06e546aac413ff
garaemon <garaemon@gmail.com>
2010-03-01 02:59:08 +0900

    add dot.emacs, but not works now

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-
+INSTALL_DIR=$HOME/.emacs.d/
 AUTO_INSTALL_URL=http://www.emacswiki.org/emacs/download/auto-install.el
 wget $AUTO_INSTALL_URL
+
+# install auto-install
+mkdir -p $INSTALL_DIR/auto-install
+mv auto-install.el $INSTALL_DIR/auto-install
+
