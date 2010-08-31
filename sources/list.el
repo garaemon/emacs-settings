4375d7b549d598ebe3aa2b3ba86fd76c5340d253
garaemon <garaemon@gmail.com>
2010-08-31 16:23:04 +0900

    fix the previous commit mistake

--- sources/list.el
+++ sources/list.el
@@ -73,10 +73,11 @@
            nil
            ("cd remember-2.0 && EMACS=$EMACS make"))
  (muse library
-       (tar-ball http://mwolson.org/static/dist/muse-latest.tar.gz)
+       (tar-ball 
+	http://download.gna.org/muse-el/muse-3.20.tar.gz)
        "a publishing environment for Emacs."
        nil
-       ("cd muse-latest && EMACS=$EMACS make"))
+       ("cd muse-3.20 && EMACS=$EMACS make"))
  (emacs-wiki library
              (tar-ball http://mwolson.org/static/dist/emacs-wiki/emacs-wiki-2.72.tar.gz)
              "Implementation of a Wiki by JohnWiegley"
@@ -209,15 +210,6 @@ to write portable Emacs Lisp programs."
 and developing Python programs"
   nil
   (:byte-compile))
- (pymacs
-  library
-  (git git://github.com/pinard/Pymacs.git)
-  "Pymacs is a powerful tool which, once started from Emacs, \
-allows both-way communication between Emacs Lisp and Python."
-  nil
-  ("sudo python setup.py install"
-   ;;(:byte-compile)
-   ))
  (ipython
   library
   http://ipython.scipy.org/dist/ipython.el
@@ -238,7 +230,7 @@ python-mode"
   (:byte-compile))
  (jsk-rosemacs
   library
-  (svn https://jsk-ros-pkg.svn.sourceforge.net/svnroot/jsk-ros-pkg/trunk/jsk_ros_tools/jsk-rosemacs)
+  (svn http://jsk-ros-pkg.svn.sourceforge.net/svnroot/jsk-ros-pkg/trunk/jsk_ros_tools/jsk-rosemacs)
   "emacs utilities for ROS(Robot Operating System) written by R.Ueda(garaemon)."
   (anything auto-complete)              ;for popup.el
   (:byte-compile))
@@ -248,17 +240,17 @@ python-mode"
   "visible, buffer local, bookmarks"
   nil
   (:byte-compile))
- (svn-clients-patched
-  library
-  (git
-   git://github.com/garaemon/svn-client.git)
-  "vc-svn, dsvn and psvn for svn client on emacs"
-  nil
-  (:byte-compile))
  (svn-clients
   library
   (svn
-   http://svn.apache.org/repos/asf/subversion/trunk/contrib/client-side/emacs) 
+   http://svn.apache.org/repos/asf/subversion/trunk/contrib/client-side/emacs)
+  "vc-svn, dsvn and psvn for svn client on emacs"
+  nil
+  (:byte-compile))
+  (svn-clients-patched
+  library
+  (git
+   git://github.com/garaemon/svn-client.git)
   "vc-svn, dsvn and psvn for svn client on emacs"
   nil
   (:byte-compile))
